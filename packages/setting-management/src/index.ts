831c90f96f022fb46e25295488a8936d2131cc27
Abdulkarim Itani <aitani@tekthar.com>
2026-02-01 00:50:15 +0300

    upgrade(setting-management): @abpjs/setting-management to v2.0.0 (#99) (#100)

--- packages/setting-management/src/index.ts
+++ packages/setting-management/src/index.ts
@@ -1,7 +1,10 @@
 /**
  * @abpjs/setting-management
  * ABP Framework Setting Management module for React
- * Translated from @abp/ng.setting-management v1.1.0
+ * Translated from @abp/ng.setting-management v2.0.0
+ *
+ * Changes in v2.0.0:
+ * - Version bump only (no code changes from v1.1.0)
  */
 
 // Models
