43bec78b442922e97a22bfba2aacc8133c02d08a
Abdulkarim Itani <aitani@tekthar.com>
2026-02-01 22:55:30 +0300

    upgrade(setting-management): @abpjs/setting-management to v2.2.0 (#155) (#156)

--- packages/setting-management/src/index.ts
+++ packages/setting-management/src/index.ts
@@ -1,7 +1,12 @@
 /**
  * @abpjs/setting-management
  * ABP Framework Setting Management module for React
- * Translated from @abp/ng.setting-management v2.1.0
+ * Translated from @abp/ng.setting-management v2.2.0
+ *
+ * Changes in v2.2.0:
+ * - Angular: Removed Router dependency from SettingManagementComponent constructor
+ * - React: No changes needed (already uses React Router hooks idiomatically)
+ * - Dependency updates to @abp/ng.theme.shared v2.2.0
  *
  * Changes in v2.0.0:
  * - Version bump only (no code changes from v1.1.0)
