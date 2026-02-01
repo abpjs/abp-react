aa2134e315040afad63add18800a364684c9ada3
Kareem <aitani@tekthar.com>
2026-02-01 09:18:37 +0300

    upgrade(account): @abpjs/account to v2.1.0 (#119)

--- packages/account/src/index.ts
+++ packages/account/src/index.ts
@@ -1,14 +1,15 @@
 /**
  * @abpjs/account
  * ABP Framework Account module for React
- * Translated from @abp/ng.account v2.0.0
+ * Translated from @abp/ng.account v2.1.0
  *
- * @version 2.0.0
+ * @version 2.1.0
  * @since 2.0.0 - Added Account namespace with component interface types
  * @since 2.0.0 - Added isSelfRegistrationEnabled support in Login/Register components
  * @since 2.0.0 - Added enableLocalLogin support in AuthWrapper component
  * @since 2.0.0 - Removed deprecated ACCOUNT_ROUTES (use AccountProvider instead)
  * @since 2.0.0 - TenantBoxComponent and AccountService now publicly exported
+ * @since 2.1.0 - Version bump only (dependency updates to @abp/ng.theme.shared v2.1.0)
  */
 
 // Models
