8e4eca361fda2c111ff8a889171849cb6a521b52
Abdulkarim Itani <aitani@tekthar.com>
2026-02-01 22:37:29 +0300

    upgrade(feature-management): @abpjs/feature-management to v2.2.0 (#149) (#150)

--- packages/feature-management/src/index.ts
+++ packages/feature-management/src/index.ts
@@ -1,9 +1,13 @@
 /**
  * @abpjs/feature-management
  * ABP Framework Feature Management module for React
- * Translated from @abp/ng.feature-management v2.1.0
+ * Translated from @abp/ng.feature-management v2.2.0
  *
- * @version 2.1.0
+ * Changes in v2.2.0:
+ * - Dependency updates to @abp/ng.theme.shared v2.2.0
+ * - No functional code changes
+ *
+ * @version 2.2.0
  * @since 2.0.0 - Added FeatureManagementComponentInputs/Outputs interfaces
  * @since 2.1.0 - Version bump only (dependency updates to @abp/ng.theme.shared v2.1.0)
  */
