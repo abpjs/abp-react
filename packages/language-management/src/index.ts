1378b4c4761aa5d55ca34073c979b86e52bccc7b
Abdulkarim Itani <aitani@tekthar.com>
2026-02-01 23:36:20 +0300

    upgrade(language-management): @abpjs/language-management to v2.2.0 (#165) (#166)

--- packages/language-management/src/index.ts
+++ packages/language-management/src/index.ts
@@ -1,7 +1,12 @@
 /**
  * @abpjs/language-management
  * ABP Framework Language Management module for React
- * Translated from @volo/abp.ng.language-management v2.1.1
+ * Translated from @volo/abp.ng.language-management v2.2.0
+ *
+ * Changes in v2.2.0:
+ * - Dependency updates to @abp/ng.theme.shared v2.2.0
+ * - Dependency updates to @volo/abp.ng.language-management.config v2.2.0
+ * - No functional code changes
  *
  * Changes in v2.1.1:
  * - Dependency updates to @abp/ng.theme.shared v2.1.0
@@ -9,7 +14,7 @@
  * - No functional code changes
  *
  * @since 2.0.0
- * @updated 2.1.1
+ * @updated 2.2.0
  */
 
 // Models
