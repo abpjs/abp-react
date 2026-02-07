ed17c901244acf3d88fd106361d1e887d6b418d1
Abdulkarim Itani <aitani@tekthar.com>
2026-02-07 05:03:06 +0300

    upgrade(text-template-management): @abpjs/text-template-management to v3.1.0 (#325) (#326)

--- packages/text-template-management/src/models/index.ts
+++ packages/text-template-management/src/models/index.ts
@@ -2,6 +2,7 @@
  * Text Template Management Models
  * @since 2.7.0
  * @updated 3.0.0 - Added config-options
+ * @updated 3.1.0 - Added GetTemplateDefinitionListInput, createGetTemplateDefinitionListInput
  */
 export * from './config-options';
 export * from './text-template-management';
